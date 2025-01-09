1f3016b9631925b84fabd809d1f9fcb58028d2d2
Tommaso Gradassi <tommasogradassi811@gmail.com>
2025-01-09 10:52:52 +0100

    Primo commit

---
+++ src/gradassi_giocosedie/Posto.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gradassi_giocosedie;
+
+class Posto
+
+{
+	private boolean occupato;
+
+	public Posto()
+
+	{
+		occupato = false;
+	}
+
+	public synchronized boolean libero() {
+		return (!occupato);
+	}
+
+	public synchronized boolean occupa() {
+		if (occupato)
+			return false;
+		else
+			return occupato = true;
+	}
+}
